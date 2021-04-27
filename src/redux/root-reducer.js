5a0e48059ef714261599f3ff4a14cf27414c8440
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-04-27 14:12:51 +0700

    updating our app to redirect to home if the user is signed in

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import userReducer from './user/user.recuder';
+import userReducer from './user/user.reducer';
 
 export default combineReducers({
   user: userReducer,
