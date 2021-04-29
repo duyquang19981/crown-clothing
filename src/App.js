bf5cd521828b7b0a6e71d3ef8dc9264dc7081c8f
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-04-29 15:59:07 +0700

    added cartIcon and cartDropdown components to our app, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -70,8 +70,5 @@ const mapStateToProps = ({ user }) => ({
   currentUser: user.currentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/4.3 crown.svg.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'></Logo>
@@ -26,14 +28,17 @@ const Header = ({ currentUser }) => (
         <Link className='option' to='/signin'>
           SIGN IN
         </Link>
-      )} 
+      )}
+      <CartIcon />
     </div>
+
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
- 
